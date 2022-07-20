756ca5014fb4ddc29a4184ebdaffc4be616c822e
Byt1k <byt1kip82@gmail.com>
2022-07-20 15:33:57 +0300

    first routes

--- 02/src/index.ts
+++ 02/src/index.ts
@@ -2,9 +2,88 @@ import express from 'express'
 const app = express()
 const port = 5000
 
+app.use(express.json())
+
+const db = {
+    courses: [
+        {id: 1, title: 'front-end'},
+        {id: 2, title: 'back-end'},
+        {id: 3, title: 'web-design'},
+        {id: 4, title: 'devops'}
+    ]
+}
+
 app.get('/', (req, res) => {
     res.send('OK!')
 })
+
+app.get('/courses', (req, res) => {
+    let foundCourses = db.courses
+
+    if (req.query.title) {
+        foundCourses = foundCourses.filter(c => c.title.indexOf(req.query.title as string) > -1)
+    }
+
+    res.json(foundCourses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id)
+
+    if (!foundCourse) {
+        res.sendStatus(404)
+        return;
+    }
+
+    res.json(foundCourse)
+})
+
+app.post('/courses', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400)
+        return;
+    }
+
+    const newId = db.courses.length + 1
+    const newCourse = {
+        id: newId,
+        title: req.body.title
+    }
+
+    db.courses.push(newCourse)
+    res.status(200).json(newCourse)
+})
+
+app.put('/courses/:id', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400)
+        return;
+    }
+
+    let foundCourse = db.courses.find(c => c.id === +req.params.id)
+
+    if (!foundCourse) {
+        res.sendStatus(404)
+        return;
+    }
+
+     foundCourse.title = req.body.title
+    res.status(200).json(foundCourse)
+})
+
+app.delete('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id)
+
+    if (!foundCourse) {
+        res.sendStatus(404)
+        return;
+    }
+
+    db.courses = db.courses.filter(c => c !== foundCourse)
+
+    res.json(foundCourse)
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
